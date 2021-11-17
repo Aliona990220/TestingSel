933f224fbbdfeee536553f37954038588f666091
Aliona990220 <aliona.havela@gmail.com>
2021-11-17 21:01:55 +0200

    first commit

---
+++ src/main/java/MeinClass.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MeinClass {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "C:\\WebDrayver\\chromedriver.exe");
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/documentation/getting_started/installing_browser_drivers/");
+        driver.close();
+    }
+}
