295e07e706c2225893a2fa8af4a05c1af3f87d16
Aliona990220 <aliona.havela@gmail.com>
2021-11-18 23:06:17 +0200

    Broken links

--- src/main/java/Find.java
+++ src/main/java/Find.java
@@ -7,7 +7,8 @@ public class Find {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver(); driver.get("https://www.softwaretestinghelp.com/");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.softwaretestinghelp.com/");
         WebElement textDemo = driver.findElement(By.xpath("//*[text()='Write and Earn']"));
         if(textDemo.isDisplayed()) {
             System.out.println("Element found using text");
