d6128b9c0de74a70efd7b4b978a346d631fbb20e
Aliona990220 <aliona.havela@gmail.com>
2021-12-14 00:01:03 +0200

    new

--- src/main/java/WheitTeme.java
+++ src/main/java/WheitTeme.java
@@ -1,16 +1,24 @@
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FacebookText {
+public class WheitTeme {
     public static void main(String[] args) {
         String baseUrl = "http://www.facebook.com/";
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-
         driver.get(baseUrl);
-        driver.findElement(By.linkText("Вход")).click();
-        System.out.println("title of page is: " + driver.getTitle());
+        driver.manage().window().setSize(new Dimension(1280, 970));
+
+
+        WebElement element = driver.findElement(By.id("email"));
+        Assert.assertTrue(element.isDisplayed());
         driver.quit();
+
     }
-}
+
+    }
+
