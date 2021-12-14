e768bf0022498c7dc30ace20a8c090bc2daf3676
Aliona990220 <aliona.havela@gmail.com>
2021-12-14 14:05:04 +0200

    buttons

---
+++ src/main/java/WikiBut.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class WikiBut {
+    public static void main(String[] args) {
+        String baseUrl = "https://ru.wikipedia.org/wiki/";
+        System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(baseUrl);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
+        driver.findElement(By.xpath("//input[@id='searchButton']")).click();
+    }
+}
