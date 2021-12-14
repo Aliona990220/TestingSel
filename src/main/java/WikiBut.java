6beaa2e74a81f4bc356f1daf57ba63f040e19a35
Aliona990220 <aliona.havela@gmail.com>
2021-12-14 14:20:57 +0200

    button.submit()

--- src/main/java/WikiBut.java
+++ src/main/java/WikiBut.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -12,5 +13,9 @@ public class WikiBut {
         driver.get(baseUrl);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
         driver.findElement(By.xpath("//input[@id='searchButton']")).click();
+
+        driver.get("https://github.com");
+        WebElement button = driver.findElement(By.xpath("//form[@class='mx-auto mx-md-0 col-5-max js-signup-form position-relative z-2']//button"));
+        button.submit();
     }
 }
